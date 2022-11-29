472ff993f83ae5c0511a9fe4946181ddb459c3ab
He1pa <18012015693@163.com>
2022-11-29 12:21:40 +0800

    zz

--- kclvm/src/lib.rs
+++ kclvm/src/lib.rs
@@ -1,9 +1,9 @@
 extern crate serde;
 
-use kclvm_parser::load_program;
-use kclvm_query::apply_overrides;
-use kclvm_runner::execute;
-use kclvm_runner::runner::*;
+pub use kclvm_parser::load_program;
+pub use kclvm_query::apply_overrides;
+pub use kclvm_runner::execute;
+pub use kclvm_runner::runner::*;
 
 #[no_mangle]
 pub extern "C" fn kclvm_cli_run(args: *const i8, plugin_agent: *const i8) -> *const i8 {
